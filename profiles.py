b2b94554175ba36533b1a1e798d1fd822c12a601
Adiel Felsen <adielfelsen@Adiels-MacBook-Pro.local>
2019-02-09 15:48:40 -0500

    basics of profile class

--- profiles.py
+++ profiles.py
@@ -3,12 +3,56 @@ class Profile:
         #Basic self info
         self.name = name
         self.gender = gender
+        self.genderPreference = genderPreference
+
         self.height = height
+        self.heightPreference = heightPreference
+        self.heightScale = heightScale
+
         self.weight = weight
-        self.genderPreference = lookingFor
+        self.weightRange = weightRange
+        self.weightScale = weightScale
+
+        self.age = age
+        self.ageRange = ageRange
+        self.ageScale = ageScale
+
+        self.religion = religion
+        self.religonScale = religionScale
+
+        self.politics = politics
+        self.politicsScale = politicsScale
+
+        self.introExtrovert = introExtrovert
+        self.oppIntroExtrovert = oppIntroExtrovert
+        self.introExtrovertScale = introExtrovertScale
+
+        self.smoke = smoke
+        self.smokeScale = smokeScale
+
+        self.messyNeat = messyNeat
+        self.messyNeatScale = messyNeatScale
+
+        #Random questions
+        self.dogCat = dogCat
+        self.dogCatScale = dogCatScale
 
 
-        #Ideal partner answers
+    def find_hated(self,profileList):
+
+        for profile in profileList:
+            hatred = 0
+
+            self.matchPercent.append(sum)
+
+    def find_opposite(self,profileList):
+        for profile in profileList:
+
+            #
+            if (profile.gender != self.genderPreference) or ():
+                break
 
 
-    def find_match(self,profileList):
+            hatred = 0
+
+            self.matchPercent.append(sum)
