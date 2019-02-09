843ec855a71716e110ab222b64fa72d4c993a2a9
Adiel Felsen <adielfelsen@Adiels-MacBook-Pro.local>
2019-02-09 14:55:09 -0500

    added main.py

---
+++ profiles.py
@@ -0,0 +1,14 @@
+class Profile:
+    def __init__(self):
+        #Basic self info
+        self.name = name
+        self.gender = gender
+        self.height = height
+        self.weight = weight
+        self.genderPreference = lookingFor
+
+
+        #Ideal partner answers
+
+
+    def find_match(self,profileList):
