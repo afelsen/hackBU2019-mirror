657951164ca26e74b7940001af1c73faae923734
Adiel Felsen <adielfelsen@Adiels-MacBook-Pro.local>
2019-02-09 17:36:11 -0500

    Completed find_opposite method

---
+++ main.py
@@ -0,0 +1,12 @@
+from profiles import Profile
+
+def main():
+    profile1 = Profile("joe","M","F",72,list(range(50, 90)),5,150,list(range(110, 170)), 5,20,list(range(18,50)),5,"Jewish","Christian",5,"D",5,"I","E",5,"N","Y",5,"M","M",5,"C",5)
+    profile2 = Profile("Jane","F","M",68,list(range(40,100)),5,140,list(range(0,1000)),5,19,list(range(18,24)),5,"Buddhist","Hindu",5,"I",9,"E","E",5,"Y","Y",5,"N","N",5,"D",5)
+    profile3 = Profile("Mary","F","M",62,list(range(73,77)),2,130,list(range(100, 250)), 7,18,list(range(18, 21)), 5, "Christian","Satanist",1,"R",10,"E","E",7,"N","N",5,"N","M",2,"D",4)
+    profile4 = Profile("Jackie","F","M",65,list(range(67,80)),5,180,list(range(120,300)),5,42,list(range(18,30)),5,"Muslim","Muslim",5,"R",5,"I","I",5,"N","N",5,"N","N",5,"C",5)
+
+    profileList = [profile1,profile3,profile4,profile2]
+    print(profile1.find_opposite(profileList))
+
+main()
--- profiles.py
+++ profiles.py
@@ -1,58 +1,84 @@
+class Attribute:
+    def __init__(self, yours, preference, scale):
+        self.yours = yours
+        self.preference = preference
+        self.scale = scale
+
 class Profile:
-    def __init__(self):
+    def __init__(self, name,
+    gender, genderPreference,
+    height, heightPreference, heightScale,
+    weight, weightPreference, weightScale,
+    age, agePreference, ageScale,
+    religion, religionPreference, religionScale,
+    politics, politicsScale,
+    introExtrovert, introExtrovertPreference, introExtrovertScale,
+    smoke, smokePreference, smokeScale,
+    messyNeat, messyNeatPreference, messyNeatScale,
+    dogCat, dogCatScale):
+
         #Basic self info
         self.name = name
+
         self.gender = gender
         self.genderPreference = genderPreference
 
-        self.height = height
-        self.heightPreference = heightPreference
-        self.heightScale = heightScale
 
-        self.weight = weight
-        self.weightRange = weightRange
-        self.weightScale = weightScale
+        #Basic items with scales (age,height,weight)
+        self.height = Attribute(height,heightPreference,heightScale)
+        self.weight = Attribute(weight, weightPreference, weightScale)
+        self.age = Attribute(age, agePreference, ageScale)
 
-        self.age = age
-        self.ageRange = ageRange
-        self.ageScale = ageScale
+        #Basic items with preferences
+        self.religion = Attribute(religion, religionPreference, religionScale)
+        self.politics = Attribute(politics,politics,politicsScale)
+        self.introExtrovert = Attribute(introExtrovert,introExtrovertPreference,introExtrovertScale)
+        self.smoke = Attribute(smoke, smokePreference, smokeScale)
+        self.messyNeat = Attribute(messyNeat, messyNeatPreference, messyNeatScale)
 
-        self.religion = religion
-        self.religonScale = religionScale
+        #Random
+        self.dogCat = Attribute(dogCat,dogCat, dogCatScale)
 
-        self.politics = politics
-        self.politicsScale = politicsScale
-
-        self.introExtrovert = introExtrovert
-        self.oppIntroExtrovert = oppIntroExtrovert
-        self.introExtrovertScale = introExtrovertScale
-
-        self.smoke = smoke
-        self.smokeScale = smokeScale
-
-        self.messyNeat = messyNeat
-        self.messyNeatScale = messyNeatScale
-
-        #Random questions
-        self.dogCat = dogCat
-        self.dogCatScale = dogCatScale
-
+    def __str__(self):
+        return str(self.name)
 
     def find_hated(self,profileList):
-
+        pass
         for profile in profileList:
             hatred = 0
 
             self.matchPercent.append(sum)
 
     def find_opposite(self,profileList):
-        for profile in profileList:
 
-            #
-            if (profile.gender != self.genderPreference) or ():
-                break
+        matchList = []
+        for p in range(len(profileList)):
+
+            Oppositeness = 0
+            #Break if the person is outside of your age or gender preference (or you're outside of theirs) - For these items, we do not want the opposite
+            if (profileList[p].gender != self.genderPreference) or not(profileList[p].age.yours in self.age.preference) or (self.gender != profileList[p].genderPreference) or not (self.age.yours in profileList[p].age.preference) :
+                continue
+
+            for k in self.__dict__:
+                if k == "name" or k == "gender" or k == "genderPreference":
+                    continue
+                elif k == "height" or k == "weight" or k == "age":
+                    val = abs(self.__dict__[k].yours-profileList[p].__dict__[k].yours)**2/100
+                    if val <= 5:
+                        Oppositeness += val
+                    else:
+                        Oppositeness += 5
+                else:
+                    if self.__dict__[k].yours != profileList[p].__dict__[k].yours:
+                        Oppositeness += 1
 
 
-            hatred = 0
+            matchList.append(Oppositeness)
 
-            self.matchPercent.append(sum)
+
+
+        maxMatch = 0
+        for o in matchList:
+            maxMatch = max(o,maxMatch)
+            print(o)
+        return profileList[p]
