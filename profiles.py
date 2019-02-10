688ec6c0487855792bdedb3cd4f2701921bd5e15
Richard Quinlivan <rquinli1@binghamton.edu>
2019-02-09 20:55:06 -0500

    all methods print

--- profiles.py
+++ profiles.py
@@ -8,9 +8,8 @@ class Profile:
     def __init__(self, name,
     gender, genderPreference,
     height, heightPreference, heightScale,
-    weight, weightPreference, weightScale,
     age, agePreference, ageScale,
-    religion, religionPreference, religionScale,
+    religion, religionScale,
     politics, politicsScale,
     introExtrovert, introExtrovertPreference, introExtrovertScale,
     smoke, smokePreference, smokeScale,
@@ -23,14 +22,12 @@ class Profile:
         self.gender = gender
         self.genderPreference = genderPreference
 
-
         #Basic items with scales (age,height,weight)
         self.height = Attribute(height,heightPreference,heightScale)
-        self.weight = Attribute(weight, weightPreference, weightScale)
         self.age = Attribute(age, agePreference, ageScale)
 
         #Basic items with preferences
-        self.religion = Attribute(religion, religionPreference, religionScale)
+        self.religion = Attribute(religion, religion, religionScale)
         self.politics = Attribute(politics,politics,politicsScale)
         self.introExtrovert = Attribute(introExtrovert,introExtrovertPreference,introExtrovertScale)
         self.smoke = Attribute(smoke, smokePreference, smokeScale)
@@ -43,39 +40,74 @@ class Profile:
         return str(self.name)
 
     def find_hated(self,profileList):
-        hatred = 0
-        for profile in profileList:
-            if (profileList[p].gender != self.genderPreference) or not(profileList[p].age.yours in self.age.preference) or (self.gender != profileList[p].genderPreference) or not (self.age.yours in profileList[p].age.preference) :
-                continue
-            for k in self.__dict__:
-                if k == "name" or k == "gender" or k == "genderPreference":
-                    continue
-                elif k == "height" or k == "weight" or k == "age":
-                    val = abs(self.__dict__[k].yours-profileList[p].__dict__[k].yours)**2/100
-                    if val <= 5:
-                        hatred += val
-                    else:
-                        hatred += 5
-                else:
-                    if self.__dict__[k].yours != profileList[p].__dict__[k].yours:
-                        hatred += 1
-
-
-        matchList.append(Oppositeness)
-    def find_opposite(self,profileList):
-
         matchList = []
         for p in range(len(profileList)):
+            hatred = 0
+            #checks you are the correct gender/age for the other person and vice versa
+            if (profileList[p].gender != self.genderPreference) or not(profileList[p].age.yours in self.age.preference) or (self.gender != profileList[p].genderPreference) or not(self.age.yours in profileList[p].age.preference):
+                matchList.append(0)
+                continue
+            for k in self.__dict__:
+                if k == "name" or k == "gender" or k == "genderPreference" or k == "age":
+                    continue
+                #calculates the amount of difference for hieght, weight, and age
+                elif k == "height":
+                    #if your H/W is bigger/smaller than thier preference, add the difference squared times 100
+                    if self.__dict__[k].yours > profileList[p].__dict__[k].preference[-1]:
+                        val = abs(self.__dict__[k].yours - profileList[p].__dict__[k].preference[-1])**2/100
+                        if val <= 5:
+                            hatred += val
+                        else:
+                            hatred += 5
+                    elif self.__dict__[k].yours < profileList[p].__dict__[k].preference[0]:
+                        val = abs(self.__dict__[k].yours - profileList[p].__dict__[k].preference[0])**2/100
+                        if val <= 5:
+                            hatred += val
+                        else:
+                            hatred += 5
+                    #if their H/W is bigger/smaller than your preference, add the difference squared times 100
+                    if profileList[p].__dict__[k].yours > self.__dict__[k].preference[-1]:
+                        val = abs(profileList[p].__dict__[k].yours - self.__dict__[k].preference[-1])**2/100
+                        if val <= 5:
+                            hatred += val
+                        else:
+                            hatred += 5
+                    elif profileList[p].__dict__[k].yours < self.__dict__[k].preference[0]:
+                        val = abs(profileList[p].__dict__[k].yours - self.__dict__[k].preference[0])**2/100
+                        if val <= 5:
+                            hatred += val
+                        else:
+                            hatred += 5
+                #for all other params, add the scale if different
+                else:
+                    if self.__dict__[k].preference != profileList[p].__dict__[k].yours:
+                        hatred += 1 * self.__dict__[k].scale
+                    if self.__dict__[k].yours != profileList[p].__dict__[k].preference:
+                        hatred += 1 * self.__dict__[k].scale
+            matchList.append(hatred)
+        maxMatch = 0
+        worstPerson = 0
+        for i in range(len(profileList)):
+            if matchList[i] > maxMatch:
+                maxMatch = matchList[i]
+                worstPerson = i
+            print(profileList[i].name + ":" + str(matchList[i]))
+        print(matchList)
+        return profileList[worstPerson]
 
+    def find_opposite(self,profileList):
+        matchList = []
+        for p in range(len(profileList)):
             Oppositeness = 0
             #Break if the person is outside of your age or gender preference (or you're outside of theirs) - For these items, we do not want the opposite
             if (profileList[p].gender != self.genderPreference) or not(profileList[p].age.yours in self.age.preference) or (self.gender != profileList[p].genderPreference) or not (self.age.yours in profileList[p].age.preference) :
+                matchList.append(0)
                 continue
-
             for k in self.__dict__:
-                if k == "name" or k == "gender" or k == "genderPreference":
+                if k == "name" or k == "gender" or k == "genderPreference" or k == "age":
                     continue
-                elif k == "height" or k == "weight" or k == "age":
+                #calculates the amount of difference for hieght, weight, and age
+                elif k == "height":
                     val = abs(self.__dict__[k].yours-profileList[p].__dict__[k].yours)**2/100
                     if val <= 5:
                         Oppositeness += val
@@ -84,14 +116,69 @@ class Profile:
                 else:
                     if self.__dict__[k].yours != profileList[p].__dict__[k].yours:
                         Oppositeness += 1
-
-
             matchList.append(Oppositeness)
-
-
-
         maxMatch = 0
-        for o in matchList:
-            maxMatch = max(o,maxMatch)
-            print(o)
-        return profileList[p]
+        worstPerson = 0
+        for i in range(len(profileList)):
+            if matchList[i] > maxMatch:
+                maxMatch = matchList[i]
+                worstPerson = i
+            print(profileList[i].name + ":" + str(matchList[i]))
+        print(matchList)
+        return profileList[worstPerson]
+
+    def find_friend_zone(self,profileList):
+        matchList = []
+        for p in range(len(profileList)):
+            awkwardness = 0
+            #checks you are the correct gender/age for the other person and vice versa
+            if (profileList[p].gender != self.genderPreference) or not(profileList[p].age.yours in self.age.preference) or (self.gender != profileList[p].genderPreference) or not (self.age.yours in profileList[p].age.preference):
+                matchList.append(0)
+                continue
+            for k in self.__dict__:
+                if k == "name" or k == "gender" or k == "genderPreference" or k == "age":
+                    continue
+                #calculates the amount of difference for hieght, weight, and age
+                elif k == "height":
+                    #if your H/W is bigger/smaller than thier preference, add the difference squared times 100
+                    if self.__dict__[k].yours > profileList[p].__dict__[k].preference[-1]:
+                        val = abs(self.__dict__[k].yours - profileList[p].__dict__[k].preference[-1])**2/100
+                        if val <= 5:
+                            awkwardness += val
+                        else:
+                            awkwardness += 5
+                    elif self.__dict__[k].yours < profileList[p].__dict__[k].preference[0]:
+                        val = abs(self.__dict__[k].yours - profileList[p].__dict__[k].preference[0])**2/100
+                        if val <= 5:
+                            awkwardness += val
+                        else:
+                            awkwardness += 5
+                    #if their H/W is bigger/smaller than your preference, add the difference squared times 100
+                    if profileList[p].__dict__[k].yours > self.__dict__[k].preference[-1]:
+                        val = abs(profileList[p].__dict__[k].yours - self.__dict__[k].preference[-1])**2/100
+                        if val <= 5:
+                            awkwardness += val
+                        else:
+                            awkwardness += 5
+                    elif profileList[p].__dict__[k].yours < self.__dict__[k].preference[0]:
+                        val = abs(profileList[p].__dict__[k].yours - self.__dict__[k].preference[0])**2/100
+                        if val <= 5:
+                            awkwardness += val
+                        else:
+                            awkwardness += 5
+                #for all other params, add the scale if different for you or same for them
+                else:
+                    if self.__dict__[k].preference != profileList[p].__dict__[k].yours:
+                        awkwardness += 1 * self.__dict__[k].scale
+                    if self.__dict__[k].yours == profileList[p].__dict__[k].preference:
+                        awkwardness += 1 * self.__dict__[k].scale
+            matchList.append(awkwardness)
+        maxMatch = 0
+        worstPerson = 0
+        for i in range(len(profileList)):
+            if matchList[i] > maxMatch:
+                maxMatch = matchList[i]
+                worstPerson = i
+            print(profileList[i].name + ":" + str(matchList[i]))
+        print(matchList)
+        return profileList[worstPerson]
